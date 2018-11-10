05e28af3fd70fa6be588c7d729c1121bebae8a30
Joby-Sunny <jobys39@live.com>
2018-11-11 02:45:26 +0530

    initial Commit

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import TopNavBar from '../TopNavBar';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+            <TopNavBar/>
+            <h1>And the App is working..!</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/TopNavBar/index.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import '../../styles/TopNavBar.css';
+
+export default class TopNavBar extends React.Component {
+    render(){
+        return (
+            <div className="container-fluid main_wrapper">
+                <div className="content_wrapper">
+                    <div className="left">
+                        <span className="title">Photo Vision</span>
+                    </div>
+                    <div className="right">
+                        <ul className="navlist">
+                            <li><a>About</a></li>
+                            <li><a>Events</a></li>
+                            <li><a>Gallery</a></li>
+                            <li><a>Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
